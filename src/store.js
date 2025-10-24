3378862e5c81aff180368b59eb0eb95493cb3b7c
nusratjahandina74 <dinujahan@gmail.com>
2025-10-24 21:21:12 +0600

    Done

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import  counterSlice  from './Slice/counterSlice'
+import  counterSlice  from './slice/counterSlice'
 
 export const store = configureStore({
   reducer: {
